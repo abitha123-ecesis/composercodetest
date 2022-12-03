81a32c60676fc2c25a2b0916111903133fa8ede1
abitha123-ecesis <abithaecesis@gmail.com>
2022-12-03 12:14:23 +0530

    adding some line commit1

--- index.php
+++ index.php
@@ -4,6 +4,21 @@ require 'vendor/autoload.php';
 
 use Intervention\Image\ImageManagerStatic as Image;
 
-$image = Image::make('img/homeimg.jpg')->resize(300, 200)->save('img/homeimg1.jpg',100);
+$image = Image::make('img/homeimg.jpg')->resize(300, 200)->save('img/homeimg1.jpg',100); // blue line denotes render in an exsisting line
 
 echo '<img src="img/homeimg1.jpg" alt=""/>';
+
+?>
+<!-- green line denotes adding new line -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
